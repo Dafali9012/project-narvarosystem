ce1fb2fe450b3b78229a2f3ce614f4cbc513fcfc
Daniel <Daniel.falklimberg@gmail.com>
2020-05-11 15:31:25 +0200

    userpage v01

---
+++ vue/src/views/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="user container">
+    <div class="row">
+        <div class="navbar col bg-primary">
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .container {
+        margin:0;
+    }
+    .navbar {
+        width:100vh;
+    }
+</style>
