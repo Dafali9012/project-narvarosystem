4ceb1b2ebb33531836df90061d290d8603bcdad4
Daniel <Daniel.falklimberg@gmail.com>
2020-05-12 10:35:19 +0200

    base

--- vue/src/components/Navbar.vue
+++ vue/src/components/Navbar.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="navbar">
-    <router-link class="navbar-brand ml-2" to="/">Närvarosystem</router-link>
+  <div class="navbar text-vit container-fluid">
+    <div class="row logo navrow">
+      <div class="col-8 logo d-flex">
+        <router-link class="navbar-brand logo" to="/">
+          <img src="@/assets/images/logo.png" class="logo" />
+        </router-link>
+      </div>
+      <div class="col-4 d-flex align-items-center justify-content-between">
+        <router-link to="/">
+        <p class="no-margin unselectable text-vit">Hem</p>
+        </router-link>
+        <router-link to="/">
+        <p class="no-margin unselectable text-vit">PIM</p>
+         </router-link>
+        <router-link to="/">
+        <p class="no-margin unselectable text-vit">Profil</p>
+         </router-link>
+        <router-link to="/">
+        <p class="no-margin unselectable text-vit">Logout</p>
+         </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-    .navbar {
-        height: 8vh;
-        width:100vw;
-        background-color: #28343a;
-    }
+.navrow {
+  width: 100%;
+}
+.navbar {
+  height: 8vh;
+  width: 100vw;
+  background-color: #28343a;
+}
+.logo {
+  height: 100%;
+}
 </style>
--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -2,17 +2,42 @@
   <div class="user d-flex flex-column container-fluid">
     <div class="row">
       <div clas="col">
-        <Navbar/>
+        <Navbar />
       </div>
     </div>
     <div class="content row mt-3">
-      <div class="sidebar col-3"></div>
+      <div class="sidebar col-3">
+        <div class="row d-flex flex-column align-items-center text-vit">
+          <img
+            src="https://www.fivestardestination.com/wp-content/uploads/2019/03/Just-white-square-300x300.png"
+            class="avatar rounded-circle"
+          />
+          <h5 class="unselectable">Du är inloggad som Användare</h5>
+          <div class="line rounded" />
+          <h5 class="option unselectable">Option 1</h5>
+          <h5 class="option unselectable">Option 2</h5>
+          <h5 class="option unselectable">Option 3</h5>
+        </div>
+      </div>
       <div class="main col-9"></div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.avatar {
+  margin-top: 15%;
+  margin-bottom: 6%;
+  width: 30%;
+}
+.line {
+  height: 0.2em;
+  width: 65%;
+  background-color: #44c7c9;
+}
+.option {
+  margin-top: 5%;
+}
 .user {
   height: 100vh;
 }
