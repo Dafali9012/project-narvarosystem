a378f4a7c30470293d153252b5749f428ca5a9b5
Hang <HangTran@hotmail.se>
2020-05-13 21:02:46 +0200

    Added Logout

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="home">
+  <div  class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a v-if="this.$store.state.loggedInUser.userId" href="http://localhost:8080/logout">
+          <button v-on:click="logout" type="button" class="btn btn-info border" id="logoutButton">Logga ut</button>
+        </a>
         <router-link to="/login">
           <button type="button" class="btn btn-info border" id="loginButton">Logga in</button>
         </router-link>
@@ -16,6 +19,10 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')}
+    },
 }
 </script>
