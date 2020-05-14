0ef2ee3d3b96c1a6caf1bc45ef98d5c4d48b8e71
alebal14 <alexandra.b.balogh@gmail.com>
2020-05-14 09:17:12 +0200

    table

--- vue/src/components/user/UserClasses.vue
+++ vue/src/components/user/UserClasses.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="user d-flex flex-column container-fluid">
-    <div class="row">
-      <div clas="col">
-        <Navbar />
-      </div>
-    </div>
+    
     <div class="content row mt-3">
       <UserSideBar/>
       
-      <div class="main col-9">
-        <h1>Classes</h1>
+      <div class="main col-9 mt-5">
+        <div class="user-table mt-5">
+          <div class="table-header"></div>
+          <vue-bootstrap4-table :rows="rows" :columns="columns" :config="config">
+          </vue-bootstrap4-table>
+        </div> 
       </div>
     </div>
   </div>
@@ -19,11 +19,82 @@
 <script>
 
 import UserSideBar from "@/components/user/UserSideBar.vue";
-
+import VueBootstrap4Table from 'vue-bootstrap4-table';
 
 export default {
   components: {     
-    UserSideBar    
-  }
+    UserSideBar,
+    VueBootstrap4Table    
+  },
+   data: function() {
+        return {
+            rows: [{
+                    "namn" : "Java 101",
+                    "poäng" : "50",
+                    "startdatum": "2020-09-01",
+                    "slutdatum" : "2020-10-30",
+                    "närvaro" : "95%",
+                    "lärare" : "David Glane"
+                },
+                {
+                  "namn" : "Databas hantering",
+                    "poäng" : "100",
+                    "startdatum": "2020-11-01",
+                    "slutdatum" : "2020-12-30",
+                    "närvaro" : "75%",
+                    "lärare" : "Annika Ros"
+                   
+                },
+                {
+                  "namn" : "Projektledning",
+                    "poäng" : "50",
+                    "startdatum": "2020-01-01",
+                    "slutdatum" : "2020-01-30",
+                    "närvaro" : "50%",
+                    "lärare" : "David Glane"
+                    
+                }],
+            columns: [{
+                    label: "Namn",
+                    name: "namn",                  
+                    
+                },
+                {
+                    label: "Poäng",
+                    name: "poäng",                    
+                    
+                },
+                {
+                    label: "Startdatum",
+                    name: "startdatum",
+                   
+                },
+                {
+                    label: "Slutdatum",
+                    name: "slutdatum",                 
+                },
+                {
+                    label: "Närvaro",
+                    name: "närvaro",                 
+                },
+                {
+                    label: "Lärare",
+                    name: "lärare",                 
+                }
+                ],
+            config: {
+                checkbox_rows: false,
+                rows_selectable: false,
+                pagination: false,
+                pagination_info: false,
+                show_refresh_button:  false,
+                    show_reset_button:  false,
+                global_search:  {                        
+                        visibility:  false                        
+                    },
+                card_title: "Mina Kurser"
+            }
+        }
+    },
 };
 </script>
