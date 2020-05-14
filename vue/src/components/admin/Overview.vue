008c351fe53c5e650261e3f3a72b0f49a4bddc44
Daniel <Daniel.falklimberg@gmail.com>
2020-05-14 10:24:26 +0200

    last scoped style removed

--- vue/src/components/admin/Overview.vue
+++ vue/src/components/admin/Overview.vue
@@ -1,38 +1,9 @@
 <template>
   <div class="root d-flex justify-content-center">
     <div class="main d-flex flex-column">
-      <div class="mt-5 d-flex d-row align-items-center bg-primary" style="height:5%;width:100%">
+      <div class="d-flex d-row align-items-center bg-primary">
         <p class="no-margin ml-3 text-cream unselectable">Utbildningar</p>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.root {
-  width: 100%;
-  height: 100%;
-}
-.main {
-  width: 85%;
-  margin-top: 5%;
-  margin-left: 0%;
-}
-.line {
-  height: 0.1em;
-  width: 100%;
-  background-color: #28343a;
-}
-input,
-select {
-  width: 40%;
-  margin-left: 2%;
-  margin-right: 2%;
-}
-.full-width {
-  width: 84%;
-}
-.button-create {
-  margin-right: 8%;
-}
-</style>
