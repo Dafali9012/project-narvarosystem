955824f60f522e217b5294235f243fb4140ee43d
Daniel <Daniel.falklimberg@gmail.com>
2020-05-14 16:21:27 +0200

    end of day save

--- vue/src/components/admin/ListUsers.vue
+++ vue/src/components/admin/ListUsers.vue
@@ -6,17 +6,13 @@
           <div class="d-flex align-items-center">
             <p class="no-margin ml-3 text-cream unselectable">Filtrering</p>
           </div>
-          <div class="d-flex justify-content-end align-items-center">
-            <font-awesome-icon
-              :icon="['fas','plus-circle']"
-              class="align-self-center mr-3 text-cream"
-            />
-          </div>
         </div>
-        <div class="d-flex justify-content-center bg-white">Empty</div>
+        <div class="d-flex justify-content-center bg-lightblue">
+          <p class="text-cream">Empty</p>
+        </div>
       </div>
 
-      <div class="flex-grow-1">
+      <div class="flex-grow-1 ml-3">
         <div class="d-flex justify-content-between bg-primary">
           <div class="d-flex align-items-center">
             <p class="no-margin ml-3 text-cream unselectable">Användare</p>
