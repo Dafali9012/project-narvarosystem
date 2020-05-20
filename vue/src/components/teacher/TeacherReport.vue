5b1a7db24874d6809ca89dca87b839a7dfddca98
alebal14 <alexandra.b.balogh@gmail.com>
2020-05-20 13:39:25 +0200

    teacherreport

--- vue/src/components/teacher/TeacherReport.vue
+++ vue/src/components/teacher/TeacherReport.vue
@@ -6,20 +6,111 @@
       
       <div class="main col-9">
         <h1>Rapport</h1>
+
+         <vue-bootstrap4-table ref="table"
+            :rows="rows"
+            :columns="columns"
+            :config="config"
+          ></vue-bootstrap4-table>
+
+          
+          
+        
+        <download-excel
+	class   = "btn btn-primary"
+	:data   = "rows"
+	:fields = "json_fields"
+	name    = "filename.xls">
+
+	Export
+
+</download-excel>
+        
       </div>
+      
     </div>
   </div>
 </template>
 
-
 <script>
 
+import VueBootstrap4Table from "vue-bootstrap4-table";
+import downloadExcel from 'vue-json-excel'
 import TeacherSideBar from "@/components/teacher/TeacherSideBar.vue";
 
 
 export default {
   components: {     
-    TeacherSideBar    
-  }
+    TeacherSideBar,
+    downloadExcel,
+    VueBootstrap4Table    
+  },
+ data: function() {
+    return {
+      json_fields: {
+            'Namn': 'namn',
+            'Kursnamn': 'course',
+            'Närvaro': 'närvaro'
+            
+        },
+      
+      rows: [
+        {
+          namn: "David Glane",
+          course: "Databas hantering",         
+          närvaro: "95%",
+         
+        },
+        {
+           namn: "Daniel ",
+          course: "Databas hantering",         
+          närvaro: "55%",
+        },
+        {
+           namn: "Hang",
+          course: "Databas hantering",         
+          närvaro: "65%",
+        },
+        {
+           namn: "Alexandra ",
+          course: "Databas hantering",         
+          närvaro: "10%",
+        },
+        {
+           namn: "Petru",
+          course: "Databas hantering",         
+          närvaro: "99%",
+        },
+      ],
+      columns: [
+        {
+          label: "Namn",
+          name: "namn",
+        },
+        {
+          label: "KursNamn",
+          name: "course",
+        },
+        
+        {
+          label: "Närvaro",
+          name: "närvaro",
+        },
+        
+      ],
+      config: {
+        checkbox_rows: false,
+        rows_selectable: false,
+        pagination: false,
+        pagination_info: false,
+        show_refresh_button: false,
+        show_reset_button: false,
+        global_search: {
+          visibility: false,
+        },
+        card_title: "Mina Kurser",
+      },
+    };
+  },
 };
 </script>
