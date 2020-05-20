9e8e581f827253985c7c64d0cc697ffcf7ef5369
alebal14 <alexandra.b.balogh@gmail.com>
2020-05-20 13:22:12 +0200

    delete modal from user scheme

--- vue/src/components/user/UserScheme.vue
+++ vue/src/components/user/UserScheme.vue
@@ -79,10 +79,7 @@ export default {
             UserSideBar
         },
         data:function () { return {
-          dateTime : "",
-          title : "",
-          course: "",
-          show : false,
+         
 
           events: [ 
             {
@@ -110,90 +107,13 @@ export default {
               ]       
               }  
       
-      }, methods:{
+      }, 
        
-        customEventCreation($event, dateTime ) { 
-         
-          this.show = true
-         
-          dateTime = $event
-
-          console.log(dateTime)
-
-      /*
-         
-     
-
-    // Check if date format is correct before creating event.
-    if (dateTime) {
-      this.$refs.vuecal.createEvent(
-        // Formatted start date and time or JavaScript Date object.
-        dateTime,
-        // Event duration in minutes (Integer).
-        120,
-        // Custom event props (optional).
-        { title: 'New Event', content: 'yay! 🎉', class: 'blue-event' }
-      )
-    }*/
-        },
-        close: function() {
-          this.show = false
-        },
-        commit: function(){
-          //
-        }
-           
         
-      }
+   
       
 
     
 }
 </script>
 
-<style scoped>
-.modal-mask{
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .5);
-  display: table;
-  transition: opacity .3s ease;}
-
-.modal-wrapper{
-  display: table-cell;
-  vertical-align: middle;}
-
-.modal-container{
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;}
-
-.modal-header h3{
-  
-    margin-top: 0;
-    color: #42b983;}
-
-.modal-body{
-  margin: 20px 0;}
-
-.modal-button{
-  float: right;}
-
-.modal-enter,
-.modal-leave{
-  opacity: 0;}
-
-.modal-enter .modal-container,
-.modal-leave .modal-container{
-  transform: scale(1.1);}
-
-</style>
