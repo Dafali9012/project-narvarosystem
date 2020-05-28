b8067757bb7313979fec6935d242105143fd16f4
alebal14 <alexandra.b.balogh@gmail.com>
2020-05-28 09:22:54 +0200

    conflict lösning

--- spring/src/main/java/ecproject/narvarosystem/Repository/UserRepository.java
+++ spring/src/main/java/ecproject/narvarosystem/Repository/UserRepository.java
@@ -1,24 +1,27 @@
 package ecproject.narvarosystem.Repository;
 
 import ecproject.narvarosystem.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
    //@Query(value="SELECT * FROM user WHERE first_name = :firstName", nativeQuery=true)
    // List<User> findAllByFirstName(String firstname);
     List<User> findAll();
 
     User findByEmail(String email);
 
-    /*@Query(value="SELECT TOP 1 * FROM user WHERE first_name = :firstName", nativeQuery=true)
-    Optional<User> findByFirstName(String firstName);
+    @Query(value="SELECT TOP 1 * FROM user WHERE first_name = :firstName", nativeQuery=true)
+    List<User> findByFirstName(String firstName);
 
     @Query(value="SELECT * FROM user WHERE role_id = :id", nativeQuery=true)
-    List<User> findAllByRoleId(int id);*/
+    List<User> findAllByRoleId(int id);
 
 }
