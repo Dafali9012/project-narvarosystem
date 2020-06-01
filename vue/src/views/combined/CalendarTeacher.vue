e858f6273d108d8d993f274b94fee671f1a9c615
alebal14 <alexandra.b.balogh@gmail.com>
2020-06-01 09:31:21 +0200

    create role

--- vue/src/views/combined/CalendarTeacher.vue
+++ vue/src/views/combined/CalendarTeacher.vue
@@ -7,7 +7,7 @@
       <div class="col-9 d-flex">
         <div class="root d-flex flex-column container-fluid">
           <div class="content row mt-3">
-            <UserSideBar />
+           
 
             <div class="main col-9 mt-5">
               <div class="row ml-5">
@@ -15,83 +15,20 @@
                         <VueCal
                     class="vuecal--blue-theme"
                     ref="vuecal"
-                    :time="true"
-                    hide-weekends
+                    :time="false"
+                    hide-weekends                    
                     show-week-numbers
                     active-view="week"
-                    :disable-views="['years', 'year', 'month']"
+                    :disable-views="['years', 'year', 'month', 'day']"
                     style="height: 500px"
                     :cell-click-hold="false"
                     editable-events
-                    :events="events"
+                    :events="events"                    
                   ></VueCal>
                 </div>
                 <div class="col-6">
                   <h1 class="d-flex text-align-left"> Lägg till</h1>
-                  <form>
-                    <div class="form-group">
-                      <label class="presenceLabel" for="inputname"></label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="Ititle"
-                        id="inputname"
-                        placeholder="Namn"
-                      />
-                    </div>
-                    <div class="form-group">
-                      <label class="presenceLabel" for="inputstart"></label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="Istart"
-                        id="inputstart"
-                        placeholder="Start"
-                      />
-                    </div>
-                    <div class="form-group">
-                      <label class="presenceLabel" for="inputend"></label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="Iend"
-                        id="inputend"
-                        placeholder="Slut"
-                      />
-                    </div>
-                    <div class="form-group">
-                      <label class="presenceLabel" for="inputcontent"
-                        ></label
-                      >
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="Icontent"
-                        id="inputcontent"
-                        placeholder="Beskrivning"
-                      />
-                    </div>
-                    <div class="form-group">
-                      <label class="presenceLabel" for="inputcourse"></label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="Icourse"
-                        id="inputcourse"
-                        placeholder="Kurs"
-                      />
-                    </div>
-                    <button v-on:click="
-                      events.push({
-                        start: Istart,
-                        end: Iend,
-                        title: Ititle,
-                        course: Icourse,
-                        course: Icontent,
-                        class: 'lec',
-                      })
-                    " type="submit" class="button button-primary"> <span> Skapa </span> </button>
-                  </form>
+                  
                 </div>
               </div>
             </div>
@@ -113,43 +50,31 @@ export default {
   },
   data: function() {
     return {
-     /*events: [
-        {
-          start: "2020-05-15",
-          end: "2020-05-15",
-          title: "Lektion 101",
-          content: "Lektion Om....",
-          course: "Java 101"
-        },
-        {
-          start: "2020-05-22",
-          end: "2020-05-22",
-          title: "Lektion 102",
-          content: "Lektion Om....",
-          course: "Java 101"
-        },
-        {
-          start: "2020-05-25",
-          end: "2020-05-25",
-          title: "Lektion 103",
-          content: "Lektion Om....",
-          course: "Java 101"
-        }
-      ]*/
+    // events: [
+       // {
+          //start: this.$store.state.MyLectureTeacher.date,          
+          //title: "Lecktion " + this.$store.state.MyLectureTeacher.id,
+          //content: "Lektion Om....",
+          //course: "Java 101"
+      //  }
+        
+     // ]
     };    
   },mounted() {    
     this.$store.dispatch("getMyLectureTeacher", this.$store.state.loggedInUser.id);
-  },
+  }, 
   computed:{
-     events: {
-       get(){
-         
+   events: {
+       get(){    
 
-          return 'start ' + this.$store.state.MyLectureTeacher.date + 'end' + this.$store.state.MyLectureTeacher.date + 'title' + this.$store.state.MyLectureTeacher.date.id
+        let myevents = this.$store.state.MyLectureTeacher
+
+        
+         
+        return myevents
        }
      }
   },
- 
    
 
 };
--- vue/src/views/combined/CreateRole.vue
+++ vue/src/views/combined/CreateRole.vue
@@ -10,31 +10,63 @@
             <h2 class="align-self-start">Skapa ny roll</h2>
             <div class="content-line rounded" />
 
-            <form @submit.prevent="createClass">
+            <form @submit.prevent="createRole">
               <div class="mt-5 d-flex justify-content-center">
-                <select class="form-control width-42" id="education" name="education">
-                  <option
-                    :value="education.id"
-                    v-for="education in getEducations"
-                    :key="education.id"
-                  >{{education.name}}</option>
-                </select>
-                <select class="form-control" id="manager" name="manager">
-                  <option
-                    :value="ecPers.id"
-                    v-for="ecPers in getEcPersonnel"
-                    :key="ecPers.id"
-                  >{{ecPers.userec.first_name}} {{ecPers.userec.last_name}}</option>
-                </select>
               </div>
+                
               <div class="mt-4 d-flex justify-content-center">
                 <input
                   class="form-control width-84"
                   type="text"
-                  placeholder="Klassnamn"
-                  id="class-name"
+                  placeholder="Ny Roll"
+                  id="role_name"
                 />
               </div>
+
+              <div class="mt-4 d-flex justify-content-center">
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="checkbox" id="create_class">
+                  <label class="form-check-label" for="create_class">Skapa Klass</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"  type="checkbox" id="create_account">
+                  <label class="form-check-label" for="create_account">Skapa Konto</label>
+                </div> 
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"   type="checkbox" id="create_course">
+                  <label class="form-check-label" for="create_course">Skapa Kurs</label>
+                </div>   
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="checkbox" id="create_education" >
+                  <label class="form-check-label" for="create_ceducation">Skapa Utbildning</label>
+                </div> 
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"  type="checkbox" id="create_role" >
+                  <label class="form-check-label" for="create_role">Skapa Roll</label>
+                </div>  
+                                           
+              </div>
+
+              <div class="mt-4 d-flex justify-content-center">  
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"   type="checkbox" id="access_overview" >
+                  <label class="form-check-label" for="create_overview">Skapa Overview</label>
+                </div>              
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"  type="checkbox" id="access_messages" >
+                  <label class="form-check-label" for="create_message">Se Meddelande</label>
+                </div>    
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"  type="checkbox" id="access_classes_courses" >
+                  <label class="form-check-label" for="access_classes_courses">Se kurser/klasser</label>
+                </div>    
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input"  type="checkbox" id="access_calendar" >
+                  <label class="form-check-label" for="access_calendar">Se kalendar</label>
+                </div>          
+              </div>
+
+
               <div class="button-create mt-4 d-flex justify-content-end">
                 <button type="submit" class="btn btn-primary">Skapa</button>
               </div>
@@ -50,29 +82,50 @@ import CombinedSidebar from "@/components/CombinedSidebar.vue";
 
 export default {
   created() {
-    this.$store.dispatch("getAllEducations");
-    this.$store.dispatch("getEcPersonnel");
+    
   },
   components: {
     CombinedSidebar
   },
   computed: {
-    getEcPersonnel() {
-      return this.$store.state.ecPersonnel;
-    },
-    getEducations() {
-      return this.$store.state.AllEducation;
-    }
+    
   },
   methods: {
-    async createClass() {
+    async createRole() {  
+      
+
+      let item = document.getElementsByClassName('form-check-input');
+      let i;
+      for (i = 0; i < item.length; i++) {
+      if (item[i].checked) {
+          item[i].setAttribute('value', "true")
+      } else {
+          item[i].setAttribute('value', "false");
+      }
+      }
+    
+      
+
       let newRole = {
-        name: document.getElementById("class-name").value,
-        education_id: document.getElementById("education").value,
-        manager_id: document.getElementById("manager").value
+        //name: document.getElementById("role_name").value,
+        //create_class: document.getElementById("create_class").value
+        name: document.getElementById("role_name").value,
+        create_class: document.getElementById("create_class").value,
+        create_account: document.getElementById("create_account").value,
+        create_course: document.getElementById("create_course").value,
+        create_education: document.getElementById("create_education").value,
+        create_role: document.getElementById("create_role").value,
+        access_overview: document.getElementById("access_overview").value,
+        access_messages: document.getElementById("access_messages").value,
+        access_classes_courses: document.getElementById("access_classes_courses").value,
+        access_calendar: document.getElementById("access_calendar").value,       
       };
 
-      let response = await fetch("http://localhost:8080/rolo", {
+
+
+      console.log(this.newRole);
+
+      let response = await fetch("http://localhost:8080/role", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newRole)
