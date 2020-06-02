9c587e719b95b4817774d1e6249162d2b02f09a3
David Glane <david@glane.net>
2020-06-02 22:08:49 +0200

    better than nothing

--- vue/src/views/combined/404.vue
+++ vue/src/views/combined/404.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="root d-flex flex-column container-fluid">
-    <div class="content row mt-3">      
+    <div class="content row mt-3 justify-content-center">
       <div class="col-9 d-flex">
         <div class="root d-flex flex-column container-fluid">
-          <div class="content row mt-3">
-              <div class="main col-9 mt-5">
-              <div class="row ml-5">
-              <h1>404</h1>
+          <div class="content row mt-3 justify-content-center">
+            <div class="main col-9 mt-5">
+              <h2>Hur har du det med navigation?</h2>
+              <p>Sidan du letar efter finns inte, har blivit borttagen, bytat namn eller är temporärt inaktiv</p>
+              <img
+                class="effect"
+                draggable="false"
+                src="https://easylaser.com/Files/Images/404-se.png"
+                style="user-select: none; width: 300px; height: 200px;"
+              />
+            </div>
           </div>
-        </div>
-        </div>
         </div>
       </div>
     </div>
-  </div>        
+  </div>
 </template>
+
+<style>
+</style>
